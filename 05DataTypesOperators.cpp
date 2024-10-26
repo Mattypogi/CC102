7b4c09b043315774c7cd23816a9b68cf08178358
Mattypogi <jjaammeess1427@gmail.com>
2024-10-26 10:50:35 +0800

    Add files via upload

---
+++ 05DataTypesOperators.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	double x1, x2, y1, y2, distance, distance_x, distance_y;
+	cout << "Enter the x coordinate of the first point: ";
+	cin >> x1;
+	cout << "Enter the y coordinate of the first point: ";
+	cin >> y1;
+	cout << "Enter the x coordinate of the 2nd the point: ";
+	cin >> x2;
+	cout << "Enter the y coordinate of the 2nd the point: ";
+	cin >> y2;
+	distance_x = x2 - x1;
+	distance_y = y2 - y1;
+	distance = sqrt (distance_x * distance_x + distance_y * distance_y);
+	cout << "The distance between the point is " << distance << endl;
+	return 0;
+}
